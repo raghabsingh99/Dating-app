38cec1c93ab843cfca86e9e5ca96f0e54bdf7427
raghab singh <raghabsingh99@gmail.com>
2024-08-05 23:19:04 -0500

    commit

---
+++ src/com/bharat/dating/model/UserAccount.java
@@ -0,0 +1,24 @@
+package com.bharat.dating.model;
+
+public class UserAccount {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public java.lang.Object getUserName() {
+        return userName;
+    }
+
+    public void setUserName(java.lang.String userName) {
+        this.userName = userName;
+    }
+
+    private String userName;
+}
